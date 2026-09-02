a73743f2f39cfc4c8929b08195018d3802cca909
coulibaly aicha leila <aichaleilacoulibaly4@gmail.com>
2026-09-02 14:50:48 +0000

    Ajout de remember pour pouvoir conserver la valeur

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -19,7 +19,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -78,10 +81,10 @@ fun TipTimeLayout() {
 @SuppressLint("UnrememberedMutableState")
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
-    var amountInput = mutableStateOf("0")
+    var amountInput by remember { mutableStateOf("") }
     TextField(
-        value = amountInput.value,
-        onValueChange = {amountInput.value},
+        value = amountInput,
+        onValueChange = {amountInput = it},
         modifier = modifier
     )
 }
